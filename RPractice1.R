7b2af1ff8e117b304280f84502f9fb2f3ab2bb7f
keith fernandes <fernandeskeith9@gmail.com>
2019-01-18 20:22:54 -0500

    Practice day 1

---
+++ RPractice1.R
@@ -0,0 +1,30 @@
+
+sqrt(10)
+
+
+x?
+  
+  
+
+prop <- function(x)
+{?
+
+  max(x)/sum(x)  
+  ?rnorm
+}
+
+prop(x)
+
+
+
+uu <- matrix(25:30,nrow = 6,ncol = 2)
+uu[,2] <- 9/5*uu[,1]+32
+
+
+
+
+
+
+
+
+
